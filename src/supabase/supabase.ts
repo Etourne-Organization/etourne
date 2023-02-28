29bcf56e0b0fbf038951bded94e152341e87fe3d
Muntasir2001 <muntasirenamalhuda@gmail.com>
2023-02-28 18:45:01 +0000

    did a supabase setup, doppler setup for env

---
+++ src/supabase/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL: string | any = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY: string | any = process.env.SUPABASE_ANON_KEY;
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
