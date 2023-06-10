ade829456e94cb8e717b21c0d8e1bd425682f022
Muntasir2001 <muntasirenamalhuda@gmail.com>
2023-06-10 08:58:17 +0100

    added user role check to getEvent, add event embed to getEvent

--- src/slash-commands/commands/getEvent.ts
+++ src/slash-commands/commands/getEvent.ts
@@ -1,10 +1,24 @@
 import fs from 'fs';
 
-import { BaseCommandInteraction, Client, MessageEmbed } from 'discord.js';
-import { getAllColumnValueById } from '../../supabase/supabaseFunctions/events';
+import {
+	BaseCommandInteraction,
+	Client,
+	MessageEmbed,
+	MessageButton,
+	MessageActionRow,
+} from 'discord.js';
+import dayjs from 'dayjs';
 
+import {
+	getAllColumnValueById,
+	setColumnValue,
+} from '../../supabase/supabaseFunctions/events';
 import { Command } from '../CommandStructure';
 import infoMessageEmbed from '../../globalUtils/infoMessageEmbed';
+import {
+	getUserRole,
+	setUserRole as setUserRoleSupabase,
+} from '../../supabase/supabaseFunctions/users';
 
 const getEvent: Command = {
 	name: 'getevent',
@@ -22,6 +36,23 @@ const getEvent: Command = {
 		try {
 			const eventId: number | any = interaction.options.get('eventid');
 
+			const userRoleDB: any = await getUserRole({
+				discordUserId: interaction.user.id,
+				discordServerId: interaction.guild!.id,
+			});
+
+			if (userRoleDB.length === 0 || userRoleDB[0]['roleId'] !== 3) {
+				return await interaction.reply({
+					embeds: [
+						infoMessageEmbed(
+							':warning: You are not allowed to run this command!',
+							'WARNING',
+						),
+					],
+					ephemeral: true,
+				});
+			}
+
 			const eventInfo: any = await getAllColumnValueById({
 				id: eventId.value,
 			});
@@ -38,7 +69,7 @@ const getEvent: Command = {
 						.setDescription(
 							`The event embed has already been shared in <#${
 								fetchedMessage.channelId
-							}> (Message link: https://discord.com/channels/${
+							}> (message link: https://discord.com/channels/${
 								interaction.guild!.id
 							}/${fetchedMessage.channelId}/${
 								fetchedMessage.id
@@ -51,8 +82,61 @@ const getEvent: Command = {
 						ephemeral: true,
 					});
 				} else {
+					const eventEmbed = new MessageEmbed()
+						.setColor('#3a9ce2')
+						.setTitle(eventInfo[0].eventName)
+						.setDescription(
+							`**----------------------------------------** \n **Event description:** \n \n >>> ${eventInfo[0].description}  \n \n`,
+						)
+						.addFields([
+							{
+								name: 'Event date & time',
+								value: `<t:${dayjs(
+									eventInfo[0]['dateTime'],
+								).unix()}:F>`,
+								inline: true,
+							},
+							{
+								name: 'Game name',
+								value: eventInfo[0].gameName,
+								inline: true,
+							},
+							{ name: 'Hosted by', value: eventInfo[0].eventHost },
+							{
+								name: `Registered players 0/${
+									eventInfo[0].maxNumPlayers
+										? eventInfo[0].maxNumPlayers
+										: 'unlimited'
+								}`,
+								value: ` `,
+							},
+						])
+						.setFooter({
+							text: `Event ID: ${eventId.value}`,
+						});
+
+					const reply = await interaction.channel?.send({
+						embeds: [eventEmbed],
+					});
+
+					// await setColumnValue({
+					// 	data: [
+					// 		{
+					// 			id: eventId,
+					// 			key: 'messageId',
+					// 			value: reply!.id,
+					// 		},
+					// 	],
+					// });
+
 					return await interaction.reply({
-						content: 'Sharing event',
+						embeds: [
+							infoMessageEmbed(
+								':white_check_mark: Event reshared Successfully',
+								'SUCCESS',
+							),
+						],
+						ephemeral: true,
 					});
 				}
 			} else {
