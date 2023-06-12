11e561f98e061cce3d96b7b72ba4efa9631add1a
Muntasir2001 <muntasirenamalhuda@gmail.com>
2023-06-12 12:23:02 +0100

    updated down pull up script

--- down-pull-up.sh
+++ down-pull-up.sh
@@ -3,19 +3,21 @@ echo "stopping bot.js"
 # forever stopall
 pm2 stop doppler-run
 
-echo "sleeping 2sec"
+echo "sleeping 1.5sec"
+sleep 1.5
 
+echo "Building prod version"
+npm run build
+
+echo "sleeping 1.5sec"
 sleep 1.5
 
 echo "git pulling"
-
 git pull
 
-echo "sleeping 2sec"
-
+echo "sleeping 1.5sec"
 sleep 1.5
 
 echo "starting up bot.js"
-
 # forever -o out.log -e err.log ts-node ./src/bot.ts
 pm2 start bin/doppler-run.sh
