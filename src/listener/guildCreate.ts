427872c0b36a7b7c9e509759efb4e65d3300111a
Muntasir2001 <muntasirenamalhuda@gmail.com>
2024-02-21 18:48:22 +0000

    added audit log permission check to guildCreate event listener

--- src/listener/guildCreate.ts
+++ src/listener/guildCreate.ts
@@ -1,26 +1,44 @@
+import fs from 'fs';
+
 import { Guild, Client } from 'discord.js';
 
 import { checkAddUser } from '../supabase/supabaseFunctions/users';
 import { checkAddServer } from '../supabase/supabaseFunctions/servers';
 
 export default (client: Client): void => {
-	client.on('guildCreate', async (guild: Guild) => {
-		const fetchedLog = await guild.fetchAuditLogs({
-			type: 'BOT_ADD',
-			limit: 1,
-		});
-		const log = fetchedLog.entries.first();
+	try {
+		client.on('guildCreate', async (guild: Guild) => {
+			if (!guild!.members.me?.permissions.has('VIEW_AUDIT_LOG')) return;
 
-		await checkAddServer({
-			discordServerId: guild.id,
-			name: guild.name,
-		});
+			const fetchedLog = await guild.fetchAuditLogs({
+				type: 'BOT_ADD',
+				limit: 1,
+			});
+			const log = fetchedLog.entries.first();
 
-		await checkAddUser({
-			username: log!.executor!.tag,
-			discordServerId: guild.id,
-			discordUserId: log!.executor!.id,
-			roleId: 3,
+			await checkAddServer({
+				discordServerId: guild.id,
+				name: guild.name,
+			});
+
+			await checkAddUser({
+				username: log!.executor!.tag,
+				discordServerId: guild.id,
+				discordUserId: log!.executor!.id,
+				roleId: 3,
+			});
 		});
-	});
+	} catch (err) {
+		try {
+			fs.appendFile(
+				'logs/crash_logs.txt',
+				`${new Date()} : Something went wrong in listener/guildCreate.ts \n Actual error: ${err} \n \n`,
+				(err) => {
+					if (err) throw err;
+				},
+			);
+		} catch (err) {
+			console.log('Error logging failed');
+		}
+	}
 };
