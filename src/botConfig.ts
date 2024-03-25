ebe3b72a05d23a8ed766b6ff42768a36dd67b61b
Muntasir2001 <muntasirenamalhuda@gmail.com>
2024-03-25 10:45:07 +0000

    added botconfig, updated infomessageembed script

---
+++ src/botConfig.ts
@@ -0,0 +1,14 @@
+import { ColorResolvable } from 'discord.js';
+
+const botConfig = {
+	color: {
+		default: '#3A9CE2' as ColorResolvable,
+		red: '#D83C3E' as ColorResolvable,
+		green: '#3BA55C' as ColorResolvable,
+		orange: '#FFA500' as ColorResolvable,
+	},
+	spamTimeoutDuration: 15,
+	DOT: ' • ',
+};
+
+export default botConfig;
